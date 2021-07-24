6747a3591a01d4b772cac9de22de8e63695afc74
Adam-Markevych <87534391+Adam-Markevych@users.noreply.github.com>
2021-07-24 08:42:52 +0300

    Add files via upload

---
+++ zavdania2/script.js
@@ -0,0 +1,32 @@
+let password;
+let ask;
+ask = prompt("Введіть слово Імя");
+
+if (ask == "Ім'я" || ask == "ім'я") {
+   password = prompt("Введіть пароль");
+   if (password == "ЛОГОС" || password == "логос"){
+        alert("Ласкаво просимо");
+   }
+   else if (password == null){
+        alert("Вхід скасований");
+   }
+   else if (password == ""){
+    alert("Ви нічого не ввели");
+}
+   else {
+        alert("Пароль не вірний");
+   }
+}
+else if (ask == ""){
+    alert("Ви нічого не ввели");
+}
+else if (ask == null) {
+    alert("Вхід скасований");
+}
+else {
+    alert("Я вас не знаю");
+}
+
+
+
+
---
+++ zavdania3/script.js
@@ -0,0 +1,53 @@
+let season;
+
+season = prompt('Веддіть число від 1 до 12')
+
+if(season == '1')
+{
+    alert("Січень");
+}
+else if(season == '2')
+{
+    alert("Лютий");
+}
+else if(season == '3')
+{
+    alert("Березень");
+}
+else if(season == '4')
+{
+    alert("Квітень");
+}
+else if(season == '5')
+{
+    alert("Травень");
+}
+else if(season == '6')
+{
+    alert("Червень");
+}
+else if(season == '7')
+{
+    alert("Липень");
+}
+else if(season == '8')
+{
+    alert("Серпень");
+}
+else if(season == '9')
+{
+    alert("Вересень");
+}
+else if(season == '10')
+{
+    alert("Жовтень");
+}
+else if(season == '11')
+{
+    alert("Листопад");
+}
+else if(season == '12')
+{
+    alert("Грудень");
+}
+
